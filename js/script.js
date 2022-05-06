0d4131ac124a44d7b7e35a189e6ba7fdcf246637
Jonah Alvarez <85708581+jonah404@users.noreply.github.com>
2022-05-06 12:18:48 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+//******************************/
+
+// alert("script para calcular la tabla del número que elijas")
+
+// let number = parseInt(prompt("Ingrese el numero de la tabla que quiere calcular, ej. tabla del 4: "))
+
+// for(i=0; i<=10; i++){
+//     result = number * i;
+//     console.log(number + " x " + i + " = " + result);
+// }
+
+// console.log("El script finalizó")
+
+
+//************************/
+
+// alert("Debes ingresar la palabra correcta para salir del programa");
+
+// let winnerWord = prompt("Ingresa la palabra correcta para salir");
+
+// while(winnerWord != "EXIT"){
+
+//     alert("Esa palabra no es la correcta, intenta de nuevo");
+//     winnerWord = prompt("Ingresa la palabra correcta para salir");
+
+//     }
+//         if(winnerWord == "EXIT"){
+
+//         alert("GANASTE!!! " + "(" + winnerWord + ")" + " era la palabra correcta, el programa terminó")
+
+// }
+
+//******************************/
+
+// alert("Este script repite la cantidad de veces que quieras la palabra o frase que quieras ")
+
+// let wordRepeat = prompt("Ingresa una palabra o frase que quieres repetir: ")
+// let counter = 1;
+// let repeat= parseInt(prompt("Ingresa la cantidad de veces que la quieres repetir: "))
+
+// while(repeat>=counter){
+//     console.log("Repetición N° " + counter + " de la palabra " + wordRepeat)
+//     counter++;
+// }
+
+// console.log("El script terminó.")
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
